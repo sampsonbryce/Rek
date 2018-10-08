066afe1d59fd12ae429827fffde68286c01c82bb
corywall1995 <cwall4@mail.csuchico.edu>
2018-10-07 18:23:41 -0700

    Nav working with expo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,16 @@ import { View, Text} from 'react-native';
 import FindServices from 'src/screens/FindServices';
 // import Admin from 'src/screens/Admin';
 
+//For navigation
+import AppNavigator from 'src/components/nav';
+
 export default class Main extends Component{
     render(){
         return (
             <View style={{flex:1}}>
                 {/* <Signup></Signup>
                   <Login></Login> */}
-                <FindServices />
+                <AppNavigator />
                 {/* <Admin /> */}
                 {/* <Text>Hello</Text> */}
             </View>
