2174f1064568eb95bd72425a88534413e9300041
Bryce Sampson <sampson.bryce@yahoo.com>
2018-12-09 16:44:20 -0800

    fixed backend lint error

--- backend/src/resolvers/Query/EmployeeQueries.js
+++ backend/src/resolvers/Query/EmployeeQueries.js
@@ -1,11 +1,1 @@
-async function workingTimes(root, args, ctx) {
-    console.log('ARGS: ', args);
-    // return ctx.db.employee({ id: args.id }).schedule().workingTimes({where: {
-    //     start_gt: args.
-    // }});
-    return [];
-}
-
-module.exports = {
-    workingTimes,
-};
+module.exports = {};
